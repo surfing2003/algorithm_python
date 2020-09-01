9af426566232a0db4583dc417c481503e8b201ab
surfing2003 <surfing2003@naver.com>
2020-09-02 01:47:36 +0900

    greedy_06

---
+++ 01_greedy/greedy_ex00.py
@@ -0,0 +1,24 @@
+# baekjoon 1138 한줄서기
+# 작은 순서부터 위치를 정해가면 되는 문제
+# 앞에 경우는 신경 쓰지 않고 인덱스 배열에서 나의 위치값만 가져가면 되는 부분 
+# 위치값이 겹치더라도 앞서 먼저 사라진 위치값은 나의 왼쪽 결과에 아무런 영향 X
+# 
+
+#         h_chk = [-1,2,1,1,0]              index = [0,1,2,3]
+# i = 1   h_chk[i] = 2   index.pop(2) = 2   index = [0,1,3]    result = [-1,-1,1,-1]
+# i = 2   h_chk[i] = 1   index.pop(1) = 1   index = [0,3]      result = [-1,2,1,-1]
+# i = 3   h_chk[i] = 1   index.pop(1) = 3   index = [0]        result = [-1,2,1,3]
+# i = 4   h_chk[i] = 0   index.pop(0) = 0   index = []         result = [4,2,1,3]
+
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+n = int(input())
+h_chk = list(map(int,input().split()))
+result = [-1]*n
+index = list(range(n))
+
+for i in range(n):
+    result[index.pop(h_chk[i])] = i+1
+
+print(' '.join(str(e) for e in result))
