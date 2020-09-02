383a871d59963f83547574289d3d4b370830f921
surfing2003 <surfing2003@naver.com>
2020-09-03 01:18:53 +0900

    etc_00

---
+++ 00_etc/etc_ex00.py
@@ -0,0 +1,24 @@
+# baekjoon 1002 터렛 
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+n = int(input())
+
+for _ in range(n):
+    x1, y1, r1, x2, y2, r2 = map(int,input().split())
+    d = (((x1-x2)**2) + ((y1-y2)**2))**0.5
+    rs = r1 + r2
+    rm = abs(r1 - r2)
+    
+    if d == 0 :
+        if r1 == r2:
+            print(-1)
+        else:
+            print(0)
+    else:
+        if d == rs or d == rm:
+            print(1)
+        elif d < rs and d > rm:
+            print(2)
+        else:
+            print(0)
