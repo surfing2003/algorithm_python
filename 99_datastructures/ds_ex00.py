cd40544028e441ce60a0b396bbc2fb873090aa7c
surfing2003 <surfing2003@naver.com>
2020-09-04 01:07:48 +0900

    ds_00

---
+++ 99_datastructures/ds_ex00.py
@@ -0,0 +1,43 @@
+# baekjoon 9012 괄호
+
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+n = int(input())
+
+# for _ in range(n):
+#     chk = input()
+#     stack = []
+#     for i in chk:
+#         if i == "(":
+#             stack.append(i)
+        
+#         if i == ")":
+#             if not stack:
+#                 stack.append(i)
+#             elif stack[-1] == "(":
+#                 stack.pop()
+#             else:
+#                 stack.append(i) 
+
+#     if not stack:
+#         print("YES")
+#     else:
+#         print("NO")   
+
+# 실제 스택이아니라 그냥 숫자로 느낌만 가져도 충분하다 
+for _ in range(n):
+    chk = input()
+    stack = 0
+    for i in chk:
+        if i == "(":
+            stack += 1
+        else:
+            stack -= 1
+            if stack < 0:
+                break
+    if stack == 0:
+        print("YES")
+    else:
+        print("NO")   
+    
