96fdf097f3f5f05e7ec77998bfc6367cd378d8ac
surfing2003 <surfing2003@naver.com>
2020-09-07 22:32:31 +0900

    ds_01

--- 99_datastructures/ds_ex01.py
+++ 99_datastructures/ds_ex01.py
@@ -20,12 +20,12 @@ def solution(progresses, speeds):
 
 # 다른 코드
 def solution(progresses, speeds):
-    Q=[]
-    for p, s in zip(progresses, speeds):
+    answer=[]
+    for progress, speed in zip(progresses, speeds):
         # Q가 비어있거나 Q[-1]> Q의 마지막 이 작으면 append 크면 출력에 +1
-        if not Q or Q[-1][0]< -((p-100)//s):
+        if not answer or answer[-1][0]< -((progress-100)//speed):
             # 음수의 값은 남는경우까지 계산되어 나머지를 확인하고 숫자를 늘려줄 필요가 없다.
-            Q.append([-((p-100)//s),1])
+            answer.append([-((progress-100)//speed),1])
         else:
-            Q[-1][1]+=1
-    return [q[1] for q in Q]
+            answer[-1][1]+=1
+    return [q[1] for q in answer]
