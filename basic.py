f028f72251c48f65036acc126c227a9f054c092c
surfing2003 <surfing2003@naver.com>
2020-09-25 23:12:32 +0900

    study

--- basic.py
+++ basic.py
@@ -30,3 +30,145 @@
 # for i in l:
 #     print(i)
 
+#####bfs
+import queue
+n, m, v = map(int, input().split())    # 정점의 개수, 간선의 개수, 시작 번호
+
+a = [list() for _ in range(n+1)]
+for _ in range(m):
+    x, y = map(int, input().split())
+    a[x].append(y)
+    a[y].append(x)
+
+for x in a:
+    x.sort()
+
+chk = [False]*(n+1)
+
+# BFS 함수 구현 
+def bfs(start):
+    q = queue.Queue()
+    q.put(start)
+    chk[start] = True
+
+    while not q.empty():               
+        now = q.get()                 # 큐에서 가장 앞으 노드를 꺼내온다.
+        print(now, end=" ")           # 해당 노드를 방문한다는 의미로 출력한다.
+        for next in a[now]:           # 방문한 노드의 인접 노드들을 for문을 통해 next에 저장한다.
+            if not chk[next]:         # 인접 노드가 체크가 되어있지 않다면
+                chk[next] = True      # 체크를 해주고,
+                q.put(next)           # 인접 노드를 큐에 넣어준다. 
+    print()
+
+bfs(v) # bfs 함수 실행 
+
+import queue
+
+# 입력
+n, m = map(int, input().split())
+a = [list(map(int, input())) for _ in range(n)]
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+chk = [[0]* m for _ in range(n)]
+
+q = queue.Queue()
+q.put([0,0])
+chk[0][0] = 1
+while not q.empty():
+    x, y = q.get()
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if nx < 0 or nx >= n: continue  # 1. nx가 배열의 범위를 넘었는지
+        if ny < 0 or ny >= m: continue  # 1. ny가 배열의 범위를 넘었는지
+        if a[nx][ny] == 0: continue     # 2. 벽이 있는지
+        if chk[nx][ny] != 0: continue   # 3. 이미 방문 한 곳인지
+
+        chk[nx][ny] = chk[x][y] + 1
+        q.put([nx, ny])
+
+print(chk[n-1][m-1])
+
+
+
+######dfs 
+n, m, v = map(int, input().split()) #정점의 개수, 간선의 개수, 시작 번호
+
+a = [list() for _ in range(n+1)]
+for _ in range(m):
+    x, y = map(int, input().split())
+    a[x].append(y)
+    a[y].append(x)
+
+for x in a:
+    x.sort()
+
+chk = [False]*(n+1)
+
+def dfs(node):
+    chk[node] = True
+    print(node, end=" ")
+
+    for next in a[node]:
+        if not chk[next]:
+            dfs(next)
+
+dfs(v)
+print()
+
+
+#########heap
+# 모듈임포트
+import heapq
+# 힙 생성
+heap = []
+# 원소 추가
+heapq.heappush(heap,1)
+heapq.heappush(heap,4)
+# 원소 제거
+heapq.heappop(heap)
+# 원소 제거 안하고 최소값 얻기
+heap[0]
+# 기존 리스트 힙으로
+heap = [1,4,3,2,6,2,1]
+heapq.heapify(heap)
+print(heap)
+
+# 최대 힙
+nums = [4, 1, 7, 3, 8, 5]
+heap = []
+for num in nums:
+  heapq.heappush(heap, (-num, num))  # (우선 순위, 값)
+# while heap:
+#   print(heapq.heappop(heap)[1])  # index 1
+
+# k번째 최소 값
+import heapq
+def kth_smallest(nums, k):
+  heap = []
+  for num in nums:
+    heapq.heappush(heap, num)
+  kth_min = None
+  for _ in range(k):
+    kth_min = heapq.heappop(heap)
+  return kth_min
+
+# print(kth_smallest([4, 1, 7, 3, 8, 5], 3))
+
+import heapq
+
+def heap_sort(nums):
+  heap = []
+  for num in nums:
+    heapq.heappush(heap, num)
+  
+  sorted_nums = []
+  while heap:
+    sorted_nums.append(heapq.heappop(heap))
+  return sorted_nums
+
+print(heap_sort([4, 1, 7, 3, 8, 5]))
+print(heap_sort([1,4,3,2,6,2,1]))
--- pr00.py
+++ pr00.py
@@ -1,8 +1,6 @@
 import sys
 input = lambda : sys.stdin.readline().rstrip()
 
-from collections import deque
-
 # # 큐
 # queue = deque()
 # queue.append(5)
@@ -14,4 +12,142 @@ from collections import deque
 # stack.pop()
 
 
+# n = list(map(int,input()))
+# a = [0]*9
 
+# for i in n:
+#     if i != 6 and i!=9:
+#         a[i] += 1
+#     else: 
+#         a[6] += 1
+
+# a[6] = -((-a[6]) // 2)
+
+# print(max(a))
+
+# n = list(map(int,input()))
+# a = [0]*9
+
+# for i in n:
+#     if i != 6 and i!=9:
+#         a[i] += 1
+#     else: 
+#         a[6] += 1
+
+# a[6] = -((-a[6]) // 2)
+
+# print(max(a))
+# import sys
+# input = lambda: sys.stdin.readline().rstrip()
+
+# n = int(input())
+# answer =[]
+# for _ in range(n):
+#     answer.append(input())
+
+# answer.sort(key=lambda x: (len(x),x))
+# temp = ''
+# for i in answer:
+#     if temp == i:
+#         continue
+#     temp = i
+#     print(i)
+
+# import sys
+# import queue
+# input = lambda : sys.stdin.readline().rstrip()
+
+# def bfs(start):
+#     q = queue.Queue()
+#     q.put(start)
+#     chk[start] = True
+    
+#     while not q.empty():
+#         now = q.get()
+#         for next in array_graph[now]:
+#             if not chk[next]:
+#                 chk[next] = True
+#                 q.put(next)
+
+# count = 0
+# n, m = map(int,input().split())
+
+# array_graph = [list() for _ in range(n+1)]
+# chk = [False] * (n+1)
+
+# for _ in range(m):
+#     x,y = map(int,input().split())
+#     array_graph[x].append(y)
+#     array_graph[y].append(x)
+
+# for i in array_graph:
+#     i.sort()
+
+
+# for i in range(1,n+1):
+#     if not chk[i]:
+#         count += 1
+#         bfs(i)
+
+# print(count)
+
+
+# a = []
+# for _ in range(9):
+#     a.append(int(input()))
+
+# a.sort()
+# sum_a = sum(a)
+
+
+# for i in range(9):
+#     for j in range(i+1,9):
+#         if sum_a -a[i] -a[j] == 100:
+#             for k in range(9):
+#                 if k == i or k == j:
+#                     continue
+#                 else:
+#                     print(a[k])
+#             exit()
+
+
+##### 백준 7662 이중우선순위 : 다시풀어야함
+
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    max_heap, min_heap = [], []
+    del_max_heap, del_min_heap = [], []
+
+    for _ in range(int(input())):
+        c, n = input().split()
+        n = int(n)
+
+        if c == "I":
+            heapq.heappush(min_heap, n)
+            heapq.heappush(max_heap, -n)
+
+        else:
+            if n == 1:
+                if max_heap:
+                    heapq.heappush(del_max_heap, -heapq.heappop(max_heap))
+            else:
+                if min_heap:
+                    heapq.heappush(del_min_heap, -heapq.heappop(min_heap))
+
+        while max_heap and del_min_heap and max_heap[0] == del_min_heap[0]:
+            heapq.heappop(max_heap)
+            heapq.heappop(del_min_heap)
+
+        while min_heap and del_max_heap and min_heap[0] == del_max_heap[0]:
+            heapq.heappop(min_heap)
+            heapq.heappop(del_max_heap)
+
+    if not max_heap:
+        print("EMPTY")
+
+    else:
+        print(-max_heap[0], min_heap[0])
