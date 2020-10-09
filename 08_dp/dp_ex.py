02086fc1b8024360a7c3f5436e5c23b80183a968
surfing2003 <surfing2003@naver.com>
2020-10-09 17:00:49 +0900

    algo

--- 08_dp/dp_ex.py
+++ 08_dp/dp_ex.py
@@ -142,3 +142,20 @@ for i in range(2,x+1):
         d[i] = min(d[i],d[i//3]+1)
 
 print(d[x])
+
+# bj 2579
+n = int(input())
+s = [0] * 301
+d = [0] * 301
+
+for i in range(1,n+1):
+    s[i] = int(input())
+
+d[1] = s[1]
+d[2] = s[1]+s[2] 
+d[3] = max(s[2]+s[3],s[1]+s[3])
+
+for i in range(3,n+1):
+    d[i] = max(d[i-3]+s[i-1]+s[i], d[i-2]+s[i])
+
+print(d[n])
--- pr00.py
+++ pr00.py
@@ -1,3 +1,19 @@
 import sys
 input = lambda : sys.stdin.readline().rstrip()
 
+n = int(input())
+
+s = [0] * 301
+d = [0] * 301
+
+for i in range(1,n+1):
+    s[i] = int(input())
+
+d[1] = s[1]
+d[2] = s[1]+s[2] 
+d[3] = max(s[2]+s[3],s[1],s[3])
+
+for i in range(3,n+1):
+    d[i] = max(d[i-3]+s[i-1]+s[i], d[i-2]+s[i])
+
+print(d[n])
