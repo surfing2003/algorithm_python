07ea348d5c68a6c1b074da801e9421c2e4d1759d
surfing2003 <surfing2003@naver.com>
2020-10-16 03:40:36 +0900

    backtracking

--- basic.py
+++ basic.py
@@ -274,3 +274,27 @@ left = 3
 right = 4
 print(prefix_sum[right]-prefix_sum[left-1])
 
+
+
+# 백트래킹
+# 현재상태에서 가능한 모든 후보군을 따라 들어가며 탐색하는 알고리즘
+ 
+# bj 15649 N과 M(1)
+
+N, M = map(int, input().split())
+visited = [False] * N  # 탐사 여부 check
+out = []  # 출력 내용
+
+def solve(depth, N, M):
+    if depth == M:  # 탈출 조건
+        print(' '.join(map(str, out)))  # list를 str으로 합쳐 출력
+        return
+    for i in range(N):  # 탐사 check 하면서
+        if not visited[i]:  # 탐사 안했다면
+            visited[i] = True  # 탐사 시작(중복 제거)
+            out.append(i+1)  # 탐사 내용
+            solve(depth+1, N, M)  # 깊이 우선 탐색
+            visited[i] = False  # 깊이 탐사 완료
+            out.pop()  # 탐사 내용 제거
+
+solve(0, N, M)
