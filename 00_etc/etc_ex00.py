1b981c44153f37b27b0d940f7d01b8d51832e39e
surfing2003 <surfing2003@naver.com>
2020-10-16 00:02:55 +0900

    bj-14888

--- 00_etc/etc_ex00.py
+++ 00_etc/etc_ex00.py
@@ -39,3 +39,70 @@ for i in a:
     total += -(-i //c)
 
 print(total)
+
+
+# bj 14888 연산자 끼워넣기
+# 시간초과.
+# >> permutation 중복을 제거하면 해결된다.?
+from itertools import permutations
+n = int(input())
+numbers = list(map(int, input().split()))
+op = ["+","-","*","/"]
+op_c = list(map(int,input().split()))
+
+opers = []
+for i in op_c:
+    t = op.pop(0)
+    opers.extend([t]*i)
+
+permus = list(set(permutations(opers,len(opers))))
+
+max_a = -1000000001
+min_a = 1000000001
+for i in permus:
+    temp = numbers[0]
+    for j in range(1,n):
+        if i[j-1] == "+":
+            temp += numbers[j]
+        elif i[j-1] == "-":
+            temp -= numbers[j]
+        elif i[j-1] == "*":
+            temp *= numbers[j]
+        else:
+            if temp < 0:
+               temp = -((-temp)//numbers[j])
+            else:
+                temp //= numbers[j]
+    
+    max_a = max(temp,max_a)
+    min_a = min(temp,min_a)
+print(max_a)
+print(min_a)
+
+# 백트래킹?
+n = int(input())
+numbers = list(map(int, input().split()))
+opers = list(map(int,input().split()))
+max_a = -1000000001
+min_a = 1000000001
+
+def cal(i, result, plus, sub, mul, div):
+    global max_a
+    global min_a
+    if i == n:
+        max_a = max(max_a,result)
+        min_a = min(min_a,result)
+        return
+    if plus:
+        cal(i+1,result + numbers[i],plus-1,sub,mul,div)
+    if sub:
+        cal(i+1,result - numbers[i],plus,sub-1,mul,div)
+    if mul:
+        cal(i+1,result * numbers[i],plus,sub,mul-1,div)
+    if div:
+        cal(i+1,-(-result//numbers[i]) if result < 0 else result//numbers[i] ,plus,sub,mul,div-1)
+    
+
+cal(1,numbers[0],opers[0],opers[1],opers[2],opers[3])
+print(max_a)
+print(min_a)
