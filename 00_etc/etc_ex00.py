61c1d8059e3ab203e8d85bea0c947f19d2f0cf11
surfing2003 <surfing2003@naver.com>
2020-10-17 19:38:12 +0900

    bj-17144 error

--- 00_etc/etc_ex00.py
+++ 00_etc/etc_ex00.py
@@ -106,3 +106,36 @@ def cal(i, result, plus, sub, mul, div):
 cal(1,numbers[0],opers[0],opers[1],opers[2],opers[3])
 print(max_a)
 print(min_a)
+
+# bj 14889 스타트와 링크
+
+N = int(input())
+arr = [list(map(int,input().split())) for _ in range(N)]
+select = [False] * N
+
+min_a = sys.maxsize
+
+def gab(l, idx ,N):
+    global min_a
+    if l == N//2:
+        a = 0
+        b = 0
+        for i in range(N):
+            for j in range(i,N):
+                if select[i] and select[j]:
+                    a += arr[i][j]
+                    a += arr[j][i]
+                elif not select[i] and not select[j]:
+                    b += arr[i][j]
+                    b += arr[j][i]
+        min_a = min(min_a, abs(a-b))
+
+    for i in range(idx,N):
+        if select[i]:
+            continue
+        select[i] = True
+        gab(l+1,i,N)
+        select[i] = False
+
+gab(0,0,N)
+print(min_a)
--- pr00.py
+++ pr00.py
@@ -1,34 +1,86 @@
+# 17144 17143 17142 17779 17837 19236 19237 19238
+
+# 17144 미세먼지 
+# 시간초과... 
+
 import sys
 input = lambda : sys.stdin.readline().rstrip()
 
-# bj 14889 스타트와 링크
-# 시간초과
+R, C, T = map(int,input().split())
+arr = [list(map(int,input().split())) for _ in range(R)]
 
-N = int(input())
-arr = [list(map(int,input().split())) for _ in range(N)]
-select = [False] * N
+# 위치 찾기
+for i in range(R):
+    if arr[i][0] == -1:
+        air_up = i
+        air_down = i+1
+        break
 
-min_a = sys.maxsize
+# 확산방향
+di = [0,1,0,-1]
+dj = [1,0,-1,0]
 
-def gab(l, idx ,N):
-    global min_a
-    if l == N//2:
-        a = 0
-        b = 0
-        for i in range(N):
-            for j in range(N):
-                if select[i] and select[j]:
-                    a += arr[i][j]
-                elif not select[i] and not select[j]:
-                    b += arr[i][j]
-        min_a = min(min_a, abs(a-b))
+for _ in range(T):
+    # 확산먼지 저장
+    temp_arr = [[0]*C for _ in range(R)]
 
-    for i in range(idx,N):
-        if select[i]:
-            continue
-        select[i] = True
-        gab(l+1,i,N)
-        select[i] = False
+    # 확산
+    for i in range(R):
+        for j in range(C):
+            if (i == air_up or i == air_down) and j == 0:
+                continue        
+            t = 0
+            if arr[i][j] != 0:
+                for k in range(4):
+                    ni = i+di[k]
+                    nj = j+dj[k]
 
-gab(0,0,N)
-print(min_a)
+                    if 0<= ni < R and 0<= nj < C and arr[ni][nj] != -1:
+                        temp_arr[ni][nj] += arr[i][j] // 5
+                        t += 1
+                arr[i][j] -= t * (arr[i][j] // 5)
+
+    # 적용
+    for i in range(R):
+        for j in range(C):
+            arr[i][j] += temp_arr[i][j]
+
+    # 위쪽바람
+    # 열 0
+    for i in range(air_up-1,0,-1):
+        arr[i][0] = arr[i-1][0]
+    # 행 0
+    for i in range(C-1):
+        arr[0][i] = arr[0][i+1]
+
+    # 열 C
+    for i in range(0,air_up):
+        arr[i][C-1] = arr[i+1][C-1]
+
+    # 행 air_up
+    for i in range(C-1,0,-1):
+        arr[air_up][i] = arr[air_up][i-1]
+        if i == 1:
+            arr[air_up][i] = 0
+
+    # 아래쪽바람
+    # 열 0
+    for i in range(air_down+1,R-1):
+        arr[i][0] = arr[i+1][0]
+
+    # 행 마지막
+    for i in range(C-1):
+        arr[R-1][i] = arr[R-1][i+1]
+
+    # 열 C
+    for i in range(R-1,air_down,-1):
+        arr[i][C-1] = arr[i-1][C-1]
+
+    # 행 air_down
+    for i in range(C-1,0,-1):
+        arr[air_down][i] = arr[air_down][i-1]
+        if i == 1:
+            arr[air_down][i] = 0
+
+answer = sum([sum(i) for i in arr]) + 2
+print(answer)
