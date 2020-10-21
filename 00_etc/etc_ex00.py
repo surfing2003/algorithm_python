277eb924d32c617323a6bebff84471c95fd0add4
surfing2003 <surfing2003@naver.com>
2020-10-22 01:53:08 +0900

    etc

--- 00_etc/etc_ex00.py
+++ 00_etc/etc_ex00.py
@@ -158,3 +158,20 @@ def solution(board, moves):
                 break
     return answer
 
+# bj-10953
+N = int(input())
+for _ in range(N):
+    a,b = map(int,input().split(','))
+    print(a+b)
+
+# bj-11718 11719
+while True:
+    try:
+        print(input())
+    except EOFError:
+        break
+
+# bj-11023 11024
+N = int(input())
+for _ in range(N):
+    print(sum(list(map(int,input().split()))))
--- pr00.py
+++ pr00.py
@@ -4,3 +4,21 @@ input = lambda : sys.stdin.readline().rstrip()
 
 # 17142 17779 17837 19236 19237 19238
 # 17142 17837 다시풀어보기
+
+# bj-10953
+# N = int(input())
+# for _ in range(N):
+#     a,b = map(int,input().split(','))
+#     print(a+b)
+
+# bj-11718 11719
+while True:
+    try:
+        print(input())
+    except EOFError:
+        break
+
+# bj-11023 11024
+N = int(input())
+for _ in range(N):
+    print(sum(list(map(int,input().split()))))
