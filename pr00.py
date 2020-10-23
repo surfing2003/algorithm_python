4cdd3b2c5389ecafeeb064fc027c52103a97c179
surfing2003 <surfing2003@naver.com>
2020-10-23 13:28:19 +0900

    삼성 2072

--- pr00.py
+++ pr00.py
@@ -4,21 +4,10 @@ input = lambda : sys.stdin.readline().rstrip()
 
 # 17142 17779 17837 19236 19237 19238
 # 17142 17837 다시풀어보기
+# 삼성 2072
+T = int(input())
 
-# bj-10953
-# N = int(input())
-# for _ in range(N):
-#     a,b = map(int,input().split(','))
-#     print(a+b)
-
-# bj-11718 11719
-while True:
-    try:
-        print(input())
-    except EOFError:
-        break
-
-# bj-11023 11024
-N = int(input())
-for _ in range(N):
-    print(sum(list(map(int,input().split()))))
+for test_case in range(1, T + 1):
+    a = list(map(int,input().split()))
+    b = sum([i for i in a if i % 2 == 1])
+    print("#%d %d"%(test_case,b))
