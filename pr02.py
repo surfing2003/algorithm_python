8a897468a8b48231b8f390289e53e3185d87b9e4
surfing2003 <surfing2003@naver.com>
2021-04-15 19:24:51 +0900

    algo_study

--- pr02.py
+++ pr02.py
@@ -853,3 +853,43 @@ input = lambda : sys.stdin.readline().rstrip()
 #     print(-1)
 # else:
 #     print(d[M])
+
+# for _ in range(int(input())):
+#     N, M = map(int,input().split())
+#     array = list(map(int,input().split()))
+
+#     dp = []
+#     index = 0
+#     for i in range(N):
+#         dp.append(array[index:index+M])
+#         index += M
+    
+#     for j in range(1,M):
+#         for i in range(N):
+#             if i==0: left_up = 0
+#             else: left_up = dp[i-1][j-1]
+
+#             if i==N-1: left_down = 0
+#             else: left_down = dp[i+1][j-1]
+
+#             left = dp[i][j-1]
+#             dp[i][j] = dp[i][j] + max(left_up,left,left_down)
+
+#     result = 0
+#     for i in range(N):
+#         result = max(result,dp[i][m-1])
+#     print(result)
+
+#LIS
+N = int(input())
+array = list(map(int,input().split()))
+array.reverse()
+
+dp = [1]*N
+
+for i in range(1,N):
+    for j in range(0,i):
+        if array[j] < array[i]:
+            dp[i] = max(dp[i],dp[j]+1)
+    
+print(N - max(dp))
