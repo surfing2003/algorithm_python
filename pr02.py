b56fffc9f1cc839c1b2a8652dafff2968102e4f6
surfing2003 <surfing2003@naver.com>
2021-04-16 17:23:56 +0900

    algo_study

--- pr02.py
+++ pr02.py
@@ -1203,37 +1203,84 @@ input = lambda : sys.stdin.readline().rstrip()
 # 신장트리 
 # 그래프에서 모든 노드를 포함하면서 사이클이 존재하지 않는 부분 그래프
 # 최소신장트리 - 크루스칼 알고리즘 O(ElogE)
-def find_parent(parent,x):
-    if parent[x] != x:
-        parent[x] = find_parent(parent,parent[x])
-    return parent[x]
+# def find_parent(parent,x):
+#     if parent[x] != x:
+#         parent[x] = find_parent(parent,parent[x])
+#     return parent[x]
 
-def union_parent(parent,a,b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
-    if a < b:
-        parent[b] = a
-    else:
-        parent[a] = b
+# def union_parent(parent,a,b):
+#     a = find_parent(parent, a)
+#     b = find_parent(parent, b)
+#     if a < b:
+#         parent[b] = a
+#     else:
+#         parent[a] = b
 
-v, e = map(int,input().split())
-parent = [0] * (v+1)
-edges = []
-result = 0
+# v, e = map(int,input().split())
+# parent = [0] * (v+1)
+# edges = []
+# result = 0
 
-for i in range(1,v+1):
-    parent[i] = i
+# for i in range(1,v+1):
+#     parent[i] = i
 
-for _ in range(e):
-    a, b, cost = map(int, input().split())
-    edges.append((cost,a,b))
+# for _ in range(e):
+#     a, b, cost = map(int, input().split())
+#     edges.append((cost,a,b))
 
-edges.sort()
+# edges.sort()
 
-for edge in edges:
-    cost, a, b = edge
-    if find_parent(parent,a) != find_parent(parent,b):
-        union_parent(parent,a,b)
-        result += cost
-print(result)
+# for edge in edges:
+#     cost, a, b = edge
+#     if find_parent(parent,a) != find_parent(parent,b):
+#         union_parent(parent,a,b)
+#         result += cost
+# print(result)
 
+# 위상정렬 O(V+E)
+# 진입차수 : 특정한 노드로 들어오는 간선의 개수
+# 진출차수 : 특정한 노드로 나가는 간선의 개수
+# 큐를 이용하는 위상정렬알고리즘의 동작과정
+# 1. 진입차수가 0인 모든 노드를 큐에 넣는다 
+# 2. 큐가 빌때까지 다음의 과정을 반복한다. 
+#   1) 큐에서 원소를 꺼내 해당 노드에서 나가는 간선의 그래프에서 제거한다.
+#   2) 새롭게 진입하는 차수가 0이 된 노드를 큐에 넣는다.
+# 각 노드가 큐에 들어온 순서가 위상정렬을 수행한 결과와 같다.
+# 사이클이 없는 방향그래프 (DAG) 에서 가능
+
+# 모든원소를 방문하기 전에 큐가 비는경우 사이클이 존재
+# 스택을 활용한 DFS로도 가능
+
+# from collections import deque
+
+# v, e = map(int,input().split())
+
+# indegree = [0] * (v+1)
+# graph = [[] for i in range(v+1)]
+
+# for _ in range(e):
+#     a,b = map(int,input().split())
+#     graph[a].append(b)
+#     indegree[b] += 1
+
+# def topology_sort():
+#     result = []
+#     q = deque()
+    
+#     for i in range(1,v+1):
+#         if indegree[i] == 0:
+#             q.append(i)
+        
+#     while q:
+#         now = q.popleft()
+#         result.append(now)
+
+#         for i in graph[now]:
+#             indegree[i] -= 1
+#             if indegree[i] == 0:
+#                 q.append(i)
+#     for i in result:
+#         print(i,end = " ")
+#     print()
+
+# topology_sort()
