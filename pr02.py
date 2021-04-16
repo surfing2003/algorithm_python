dd7089139f18b2ff26f1df293e845805b9a5b997
surfing2003 <surfing2003@naver.com>
2021-04-16 13:56:21 +0900

    algo_study

--- pr02.py
+++ pr02.py
@@ -895,3 +895,59 @@ input = lambda : sys.stdin.readline().rstrip()
 # print(N - max(dp))
 
 
+# 다익스트라 O(V^2) 노드 개수 5000개 이하
+# 음의 간선 X
+# 그리디알고리즘
+# 출발노드 설정 > 최단거리테이블 초기화
+# > 방문하지 않은 노드 중에서 최단거리가 가장 짧은 노드 선택
+# > 해당 노드를 거쳐 다른노드로 가는 비용을 계산하여 최단거리테이블 갱신
+# > 3 ~ 4번 과정 반복
+
+# import sys
+# input = sys.stdin.readline
+# INF = int(1e9)
+
+# n, m = map(int,input().split())
+# start = int(input())
+
+# graph = [[] for i in range(n+1)]
+# visited = [False] * (n+1)
+# distance = [INF] * (n+1)
+
+# for _ in range(m):
+#     a,b,c = map(int,input().split())
+#     graph[a].append((b,c))
+
+# def get_smallest_node():
+#     min_value = INF
+#     index = 0
+#     for i in range(1,n+1):
+#         if distance[i] < min_value and not visited[i]:
+#             min_value = distance[i]
+#             index = i
+#     return index
+
+# def dijkstra(start):
+#     distance[start] = 0
+#     visited[start] = True
+
+#     for j in graph[start]:
+#         distance[j[0]] = j[1]
+    
+#     for i in range(n-1):
+#         now = get_smallest_node()
+#         visited[now] = True
+
+#         for j in graph[now]:
+#             cost = distance[now] + j[1]
+#             if cost < distance[j[0]]:
+#                 distance[j[0]] = cost
+
+
+# dijkstra(start)
+
+# for i in range(1,n+1):
+#     if distance[i] == INF:
+#         print("INF")
+#     else:
+#         print(distance[i])
