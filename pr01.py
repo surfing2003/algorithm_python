fccca5ffea4b2b2a21851b57f45c58df816cf4d7
surfing2003 <surfing2003@naver.com>
2021-04-17 20:57:31 +0900

    algo_study

--- pr01.py
+++ pr01.py
@@ -119,3 +119,18 @@
 #             dfs(x+1)
 #             sudoku[i][j] = 0
 # dfs(0)
+
+N = int(input())
+array = [list(map(int,input().split())) for _ in range(N)]
+dp = [[0]*(i+1) for i in range(N)]
+dp[0][0] = array[0][0]
+for i in range(1,N):
+    for j in range(i+1):
+        if j == 0:
+            dp[i][j] = array[i][j] + dp[i-1][j]
+        elif j == i:
+            dp[i][j] = array[i][j] + dp[i-1][j-1]
+        else:
+            dp[i][j] = array[i][j] + max(dp[i-1][j-1], dp[i-1][j])
+
+print(max(dp[N-1]))
