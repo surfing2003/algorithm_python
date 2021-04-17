9dac06d308a74e8c8a351c70a2a3946cd3a598f2
surfing2003 <surfing2003@naver.com>
2021-04-17 18:32:27 +0900

    algo_study

--- pr01.py
+++ pr01.py
@@ -1,6 +1,5 @@
-import sys
-input = lambda : sys.stdin.readline().rstrip()
-
+# import sys
+# input = lambda : sys.stdin.readline().rstrip()
 
 # def hanoi(n, before, after, temp):
 #     if n == 1:
@@ -15,3 +14,27 @@ input = lambda : sys.stdin.readline().rstrip()
 # hanoi(N,1,3,2) 
 
 
+# import sys
+# from collections import Counter
+# input = lambda : sys.stdin.readline().rstrip()
+
+
+# N = int(input())
+# num = [int(input()) for _ in range(N)]
+
+# print(round(sum(num)/N))
+
+# print(sorted(num)[N//2])
+
+# temp = Counter(num).most_common()
+# temp.sort(key=lambda x: (-x[1],x[0]))
+# if len(temp) > 1:
+#     if temp[0][1] == temp[1][1]:
+#         print(temp[1][0])
+#     else:
+#         print(temp[0][0])
+# else:
+#     print(temp[0][0])
+# print(max(num) - min(num))
+
+print(''.join(sorted(input())[::-1]))
