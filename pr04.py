0d484b1f52ee99d2d26f94ccf7bf469b187177fd
surfing2003 <surfing2003@naver.com>
2021-05-05 22:08:14 +0900

    algo_study

--- pr04.py
+++ pr04.py
@@ -444,3 +444,18 @@
 #     make_fractal(int(pow(2, i)))
 # for i in range(n):
 #     print(s[i])
+
+
+# T = int(input())
+# for _ in range(T):
+#     N = int(input())
+#     arr = [list(map(int,input().split())) for _ in range(2)]
+
+#     arr[0][1] += arr[1][0]
+#     arr[1][1] += arr[0][0]
+
+#     for j in range(2,N):
+#         arr[0][j] += max(arr[1][j-1],arr[1][j-2])
+#         arr[1][j] += max(arr[0][j-1],arr[0][j-2])
+    
+#     print(max(arr[0][N-1],arr[1][N-1]))
