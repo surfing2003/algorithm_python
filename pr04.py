195556dcdb732b9061ead67108e49614e5f5ddb4
surfing2003 <surfing2003@naver.com>
2021-05-07 22:09:40 +0900

    algo_study

--- pr04.py
+++ pr04.py
@@ -536,3 +536,19 @@
 
 # print(len(dp[N])-1)
 # print(*dp[N][::-1])
+
+# import sys
+# import heapq
+
+# numbers = int(input())
+# heap = []
+
+# for _ in range(numbers):
+#     num = int(sys.stdin.readline())
+#     if num != 0:
+#         heapq.heappush(heap, -num)
+#     else:
+#         try:
+#             print(-heapq.heappop(heap))
+#         except:
+#             print(0)
