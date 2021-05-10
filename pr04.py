b636c620500049537b66e0b621a8f1ce45df05d8
surfing2003 <surfing2003@naver.com>
2021-05-10 16:35:12 +0900

    algo_study

--- pr04.py
+++ pr04.py
@@ -567,3 +567,17 @@
 #         a = H
 #         b -= 1
 #     print(a*100 + b)
+
+
+
+# T = int(input())
+
+# for _ in range(T):
+#     k = int(input())
+#     n = int(input())
+#     people = [ i for i in range(1, n+1)]
+#     for __ in range(k):
+#         for j in range(1,n):
+#             people[j] += people[j-1]
+
+#     print(people[-1])
