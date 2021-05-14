e5ead1ffbae18d3ba6dfea59e87fc8dac40ba62f
surfing2003 <surfing2003@naver.com>
2021-05-14 16:14:29 +0900

    algo_study

--- pr04.py
+++ pr04.py
@@ -617,3 +617,13 @@
 #         for j in range(4):
 #             result = max(result, arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2])
 #     return result
+
+# from collections import deque
+
+# def rotLeft(a, d):
+#     temp = deque(a)
+#     temp.rotate(-d)
+#     return temp
+
+# def rotLeft(a, d):
+#     return a[d:]+a[:d]
