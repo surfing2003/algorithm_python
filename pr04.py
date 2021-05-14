bb19e5a2a46d88fde3e997a1cbfadfa06a2f7f3f
surfing2003 <surfing2003@naver.com>
2021-05-14 15:59:22 +0900

    algo_study

--- pr04.py
+++ pr04.py
@@ -609,3 +609,11 @@
 #         print("right")
 #     else:
 #         print("wrong")
+
+
+# def solution(arr):
+#     result = -64
+#     for i in range(4):
+#         for j in range(4):
+#             result = max(result, arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2])
+#     return result
