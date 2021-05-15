36984c33760545da534f2d17e3542e814331f888
surfing2003 <surfing2003@naver.com>
2021-05-15 19:34:00 +0900

    algo_study

--- pr04.py
+++ pr04.py
@@ -627,3 +627,17 @@
 
 # def rotLeft(a, d):
 #     return a[d:]+a[:d]
+
+# n = int(input())
+# numbers = list(map(int,input().split()))
+# answer = 0
+# for num in numbers:
+#     flag = True
+#     if num > 1:
+#         for i in range(2,num):
+#             if num % i == 0:
+#                 flag = False
+#                 break
+#         if flag:
+#             answer += 1
+# print(answer)
