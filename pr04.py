bcf86d80c0fe6915e7b8abb0324250740b235b65
surfing2003 <surfing2003@naver.com>
2021-05-16 17:48:39 +0900

    algo_study

--- pr04.py
+++ pr04.py
@@ -699,3 +699,20 @@
 #     div += 1
 # if N > 1:
 #     print(N)
+
+# # 1929
+# # 3928 ms >> 104ms 수정필요
+# def check(num):
+#     if num == 1:
+#         return False
+#     else:
+#         for i in range(2,int(num**0.5)+1):
+#             if num % i == 0:
+#                 return False
+#         return True
+
+# N,M = map(int,input().split())
+# for num in range(N,M+1):
+#     if check(num):
+#         print(num)
+
