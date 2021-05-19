6778b85d05798449993800d92b3a535e2c6b602b
surfing2003 <surfing2003@naver.com>
2021-05-19 17:45:26 +0900

    algo_study

--- pr04.py
+++ pr04.py
@@ -777,3 +777,14 @@
 #         print(n * 2)
 #     else:
 #         print((n * 2) + 1)
+
+# while True:
+#     a,b = map(int,input().split())
+#     if a == 0 and b == 0:
+#         break
+#     if b % a == 0:
+#         print("factor")
+#     elif a % b == 0:
+#         print("multiple")
+#     else:
+#         print("neither")
