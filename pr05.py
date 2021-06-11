9d0067ad32b95bbe3fc5bf44d12dacea64063871
surfing2003 <surfing2003@naver.com>
2021-06-11 15:54:03 +0900

    algo_study

--- pr05.py
+++ pr05.py
@@ -42,3 +42,15 @@
 #     print('*'*i+' '*(2*N-i*2)+'*'*i)
 # for i in range(N,0,-1):
 #     print('*'*i+' '*(2*N-i*2)+'*'*i)
+
+# N = int(input())
+
+# for i in range(N):
+#     print(' '*i+'*'*(2*(N-i)-1))
+# for i in range(N-2,-1,-1):
+#     print(' '*i+'*'*(2*(N-i)-1))
+
+# N = int(input())
+# for i in range(-N+1,N):
+#     print(i)
+#     print(' '*(N-abs(i)-1) + '*'*(2*abs(i)+1))
