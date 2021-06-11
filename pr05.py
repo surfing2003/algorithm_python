cd7842d7e7fdec4d40baa5d4962f96c2481e7b25
surfing2003 <surfing2003@naver.com>
2021-06-11 16:06:46 +0900

    algo_study

--- pr05.py
+++ pr05.py
@@ -54,3 +54,26 @@
 # for i in range(-N+1,N):
 #     print(i)
 #     print(' '*(N-abs(i)-1) + '*'*(2*abs(i)+1))
+
+# 752ms
+# N, M = map(int,input().split())
+# num = 1
+# for _ in range(N):
+#     for _ in range(M):
+#         if num % M == 0:
+#             print(num,end = '')
+#         else:
+#             print(num,end = ' ')
+#         num += 1
+#     print()
+
+# 556ms
+# N,M = map(int,input().split())
+
+# for i in range(N):
+#     print(*[ M*i+j+1 for j in range(M)])
+
+# 308ms
+# n, m = map(int, input().split())
+# for i in range(1, 1 + n*m, m):
+#     print(' '.join(map(str, range(i, i+m))))
