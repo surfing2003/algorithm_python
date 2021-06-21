377ba1837a3a6458496f5fd25bf45e4b4c0c12ed
surfing2003 <surfing2003@naver.com>
2021-06-21 16:36:11 +0900

    algo_study

--- pr05.py
+++ pr05.py
@@ -170,3 +170,20 @@
 
 # def solution(nums):
 #     return min(len(set(nums)),len(nums)//2)
+
+# K, N = map(int,input().split())
+# lans = [ int(input()) for _ in range(K)]
+# start, end = 1, max(lans)
+
+# while start <= end:
+#     mid = (start+end)//2
+#     lines = 0
+#     for lan in lans:
+#         lines += lan // mid
+
+#     if lines >= N:
+#         start = mid + 1
+#     else:
+#         end = mid - 1
+
+# print(end)
