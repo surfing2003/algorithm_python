cae0cd627ba877d021bcee19602faabb5f0d6eef
surfing2003 <surfing2003@naver.com>
2021-06-25 16:16:05 +0900

    algo_study

--- pr05.py
+++ pr05.py
@@ -275,3 +275,18 @@
 #         height = l
 # print(answer,height)
 
+
+# 일반적인 반복문
+# def solution(absolutes, signs):
+#     answer = 0
+#     for i in range(len(signs)):
+#         if signs[i]:
+#             answer += absolutes[i]
+#         else:
+#             answer -= absolutes[i]
+#     return answer
+
+# zip 함수 활용
+# def solution(absolutes, signs):
+#     return sum(absolutes if sign else -absolutes for absolutes, sign in zip(absolutes, signs))
+
