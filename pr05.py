90cc8b52b3bd399231a67d1215a82f27b2ea9660
surfing2003 <surfing2003@naver.com>
2021-07-01 17:45:51 +0900

    algo_study

--- pr05.py
+++ pr05.py
@@ -341,3 +341,32 @@
 #         else:
 #             temp.append(i)
 #     return 1 if not temp else 0
+
+from collections import deque
+
+def bfs(start,visited,arr):
+    count = 0
+    q = deque()
+    q.append((start, count))
+    while q:
+        now,c = q.popleft()
+        if visited[now] == -1:
+            visited[now] = c
+            for i in arr[now]:
+                q.append((i,c+1))
+
+def solution(n, edge):
+    answer = 0
+    visited = [-1] * (n+1)
+    arr = [[] for _ in  range(n+1)]
+    for e in edge:
+        arr[e[0]].append(e[1])
+        arr[e[1]].append(e[0])
+
+    bfs(1,visited,arr)
+    for v in visited:
+        if v == max(visited):
+            answer += 1
+    return answer
+
+print(solution(6,[[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]))
