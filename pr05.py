b680c1e5be7b377feae638ab8bf7c503d0e36c7c
surfing2003 <surfing2003@naver.com>
2021-07-05 18:52:53 +0900

    algo_study

--- pr05.py
+++ pr05.py
@@ -556,3 +556,18 @@
 #             S = set(list(range(1,21)))
 #         else:
 #             S = set()
+
+# N = int(input())
+
+# def s(n):
+#     if n == 1:
+#         return 1
+#     elif n == 2:
+#         return 2
+#     elif n == 3:
+#         return 4
+#     else:
+#         return s(n-1)+s(n-2)+s(n-3)
+
+# for _ in range(N):
+#     print(s(int(input())))
